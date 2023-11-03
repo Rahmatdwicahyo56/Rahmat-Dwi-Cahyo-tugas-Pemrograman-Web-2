d030f909f2efe8425b1dd9cf44849b3dbae8e47d
Rahmat Dwi Cahyo <148661228+RahmatDwiCahyo@users.noreply.github.com>
2023-11-03 21:01:20 +0700

    Update latihan4a.php

--- latihan4a.php
+++ latihan4a.php
@@ -5,10 +5,9 @@
 </head>
 <body>
 <?php
-// Buat array dengan warna
+
 $warna = array("hijau", "kuning", "kelabu", "merah muda", "biru");
 
-// Tampilkan teks dengan warna dari array menggunakan loop
 echo "Balonku ada lima.<br>";
 echo "Rupa-rupa warna-nya ";
 for ($i = 0; $i < count($warna); $i++) {
